f9d947b9a5583c429a4c5a8c7c4b0dbb1e182940
ruubyme <wngml1741@naver.com>
2024-06-25 16:51:45 +0900

    Feat: 로그인 함수생성

--- utils/io.js
+++ utils/io.js
@@ -1,4 +1,5 @@
 //통신 관련 함수들
+const userController = require("../Controllers/user.controller");
 
 module.exports = function (io) {
   //io관련된 모든 작업들
@@ -7,6 +8,12 @@ module.exports = function (io) {
 
     socket.on("login", async (userName, cb) => {
       //유저정보를 저장
+      try {
+        const user = await userController.saveUser(userName, socket.id);
+        cb({ ok: true, data: user });
+      } catch (error) {
+        cb({ ok: false, error: error.message });
+      }
     });
 
     socket.on("disconnect", () => {
